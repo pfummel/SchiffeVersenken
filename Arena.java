e0b3533c74a05bff3a1ef2264265d09dde8ce662
markus274 <markus.b.berning@web.de>
2016-01-07 16:08:13 +0100

    Konstruktor für die Klasse Arena hinzugefügt
    Der Konstruktor erzeugt ein Spielfeld für die Klasse Arena und
    initialisert es mit Wasser.
    Für toString kann leider nicht einfach die print-Methode übernommen
    werden, weil toString keine Ausgaben auf der Konsole machen darf.

--- Arena.java
+++ Arena.java
@@ -4,17 +4,38 @@ Die Klasse Arena vereint die gemeinsamen Aspekteder zwei Spielfelder und wird so
 public class Arena{
 
     /**
-    zweidimensionales Array
+    zweidimensionales Array, das als Spielfeld dient
     */
     public char[][] grid;
 
     /**
-    ...
+    Konstruktor, der ein Spielfeld der Groeße 10x10 erzeugt und mit Wasser initialisiert.
     */
-    public Arena(){}
+    public Arena(){
+        this.grid = new char[10][10];
+
+        for(int i = 0; i<10; i++){
+            for(int j = 0; j<10; j++){
+            
+                this.grid[i][j] = '0';
+            }
+        }
+    }
 
     /**
     ...
     */
-    //public String toString(){}
+    public String toString(){
+
+        char[] rowAlpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        
+        System.out.println("\n " + "0123456789");
+        for (int i = 0; i < grid.length; i++) {
+            //System.out.print(rowAlpha[i]);
+            for (int n = 0; n < grid.length; n++) {
+                //System.out.print(grid[i][n]);
+            }
+            //System.out.print("\n");
+        }
+    }
 }
