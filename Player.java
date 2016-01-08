0d13c8b4bbf76e6af7c6f64aad23a240aa23417d
pfummel <greenpfum-348@yahoo.de>
2016-01-08 16:11:56 +0100

    Methode isAlive() hinzugefügt

--- Player.java
+++ Player.java
@@ -3,7 +3,24 @@ Die Klasse Player, Erbe von Arena...
 */
 public class Player extends Arena{
 
-    //public boolean isAlive(){}
+    public boolean isAlive(){
+    
+        int shipCount = 0;
+        boolean isAlive = true;
+        
+        for(int i = 0; i<10; i++){
+            for(int j = 0; j<10; j++){
+                if (this.grid[i][j] == #) {
+                    shipCount++;
+                }
+            }
+        }
+        if (shipCount == 0) {
+            isAlive = false;
+        }
+        
+        return isAlive;
+    }
 
     //public boolean takeHit(int row, int column){}
 
